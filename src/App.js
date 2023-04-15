a9cb7a619f5466dc77a0c08ab205e3df623f4679
Denis belozerov <58891785+blzrov@users.noreply.github.com>
2023-04-15 13:15:25 +0500

    add form

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Second from "./pages/Second";
 import Error from "./pages/Error";
+import ApplicantProfile from "./pages/ApplicantProfile";
+import ApplicantEdit from "./pages/ApplicantEdit";
 import Container from "react-bootstrap/Container";
 
 export default function App() {
   return (
-    <Container fluid className="App">
+    <Container className="App">
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/second" element={<Second />} />
           <Route path="*" element={<Error />} />
+          <Route path="/applicant/:id" element={<ApplicantProfile />} />
+          <Route path="/applicant/edit" element={<ApplicantEdit />} />
         </Routes>
       </div>
     </Container>
