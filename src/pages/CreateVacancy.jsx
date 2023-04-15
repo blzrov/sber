114095ea1aa3de3a60527385a2e1fb6aaac9b30f
Denis belozerov <58891785+blzrov@users.noreply.github.com>
2023-04-15 19:46:38 +0500

    first post

--- src/pages/CreateVacancy.jsx
+++ src/pages/CreateVacancy.jsx
@@ -2,13 +2,42 @@ import { Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import { TextInput, Textarea, Checkbox, Button, Group, Text, rem, MultiSelect } from "@mantine/core";
 
-
 export default function CreateVacancy() {
   const [info, setInfo] = useState({});
+  function handleSubmit() {
+    console.log(info);
+  }
 
   return (
     <div>
-      <Row className="mb-4"></Row>
+      <h3>Создать новую вакансию</h3>
+      <Row className="mb-4">
+        <Col>
+          <TextInput
+            onChange={(e) => setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }))}
+            name="name"
+            placeholder="Аналитик"
+            label="Название вакансии"
+          />
+          <TextInput
+            onChange={(e) => setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }))}
+            name="salary"
+            placeholder="50000"
+            label="Зарплата"
+          />
+          <Textarea
+            autosize
+            multiple
+            onChange={(e) => setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }))}
+            name="descr"
+            placeholder="Напишите подробнее о вакансии"
+            label="Описание"
+          />
+          <Button color="green" onClick={handleSubmit}>
+            Опубликовать
+          </Button>
+        </Col>
+      </Row>
     </div>
   );
 }
