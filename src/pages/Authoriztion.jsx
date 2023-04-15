cb49d2d2397da1dacfca04c7bada4087b98373c0
DaniilSkovorodnikov <skovorodnikov.danil@mail.ru>
2023-04-15 13:16:30 +0500

    Set auth styles

--- src/pages/Authoriztion.jsx
+++ src/pages/Authoriztion.jsx
@@ -50,81 +50,111 @@ export default function Authorization() {
     <Row className="my-auto">
       <Col md={3} className="mx-auto">
         {authState === "login" && (
-          <form
-            onSubmit={authForm.onSubmit((values) => {
-              console.log(values);
-              authForm.reset();
-            })}
-          >
-            <TextInput
-              label="Логин"
-              placeholder="Ваш логин"
-              {...authForm.getInputProps("login")}
-              withAsterisk
-            />
+          <>
+            <h2 className='text-center'>Вход</h2>
+            <form
+              onSubmit={authForm.onSubmit((values) => {
+                console.log(values);
+                authForm.reset();
+              })}
+            >
+              <TextInput
+                label="Логин"
+                placeholder="Ваш логин"
+                {...authForm.getInputProps("login")}
+                withAsterisk
+                size="lg"
+                radius="md"
+              />
 
-            <TextInput
-              label="Пароль"
-              placeholder="Ваш пароль"
-              {...authForm.getInputProps("password")}
-              type="password"
-              withAsterisk
-            />
+              <TextInput
+                label="Пароль"
+                placeholder="Ваш пароль"
+                {...authForm.getInputProps("password")}
+                type="password"
+                withAsterisk
+                size="lg"
+                radius="md"
+              />
 
-            <Group className="mt-3 d-flex flex-column">
-              <Button type="submit" color="green">Войти</Button>
-              <p>
-                У вас нет аккаунта?{" "}
-                <span onClick={() => setAuthState("registration")} className="text-primary" style={{cursor: 'pointer'}}>
-                  Зарегистрируйтесь.
-                </span>
-              </p>
-            </Group>
-          </form>
+              <Group className="mt-3 d-flex flex-column">
+                <Button type="submit" color="green" className="w-100">
+                  Войти
+                </Button>
+                <p>
+                  У вас нет аккаунта?{" "}
+                  <span
+                    onClick={() => setAuthState("registration")}
+                    className="text-primary"
+                    style={{ cursor: "pointer" }}
+                  >
+                    Зарегистрируйтесь.
+                  </span>
+                </p>
+              </Group>
+            </form>
+          </>
         )}
         {authState === "registration" && (
-          <form
-            onSubmit={registrationForm.onSubmit((values) => {
-              console.log(values);
-              registrationForm.reset();
-            })}
-          >
-            <TextInput
-              label="Логин"
-              placeholder="Ваш логин"
-              {...registrationForm.getInputProps("login")}
-              withAsterisk
-            />
+          <>
+            <h2 className='text-center'>Регистрация</h2>
+            <form
+              onSubmit={registrationForm.onSubmit((values) => {
+                console.log(values);
+                registrationForm.reset();
+              })}
+            >
+              <TextInput
+                label="Логин"
+                placeholder="Ваш логин"
+                {...registrationForm.getInputProps("login")}
+                withAsterisk
+                size="lg"
+                radius="md"
+              />
 
-            <TextInput
-              label="Пароль"
-              placeholder="Ваш пароль"
-              {...registrationForm.getInputProps("password")}
-              type="password"
-              withAsterisk
-            />
+              <TextInput
+                label="Пароль"
+                placeholder="Ваш пароль"
+                {...registrationForm.getInputProps("password")}
+                type="password"
+                withAsterisk
+                size="lg"
+                radius="md"
+              />
 
-            <Select
-              label="Ваша роль"
-              placeholder="Выберите вашу роль"
-              data={[
-                { value: "employer", label: "Работодатель" },
-                { value: "applicant", label: "Соискатель" },
-                { value: "mentor", label: "Ментор" },
-                { value: "customer", label: "Заказчик" },
-              ]}
-              {...registrationForm.getInputProps("role")}
-              withAsterisk
-            />
+              <Select
+                label="Ваша роль"
+                placeholder="Выберите вашу роль"
+                data={[
+                  { value: "employer", label: "Работодатель" },
+                  { value: "applicant", label: "Соискатель" },
+                  { value: "mentor", label: "Ментор" },
+                  { value: "customer", label: "Заказчик" },
+                ]}
+                {...registrationForm.getInputProps("role")}
+                withAsterisk
+                size="lg"
+                radius="md"
+              />
 
-            <Group className="mt-3 d-flex flex-column">
-              <Button type="submit" color="green">Зарегистрироваться</Button>
-              <p>
-                У вас уже есть аккаунт?{" "}
-                <span onClick={() => setAuthState("login")} className="text-primary" style={{cursor: 'pointer'}}>Войдите.</span>
-              </p>
-            </Group>
-          </form>
+              <Group className="mt-3 d-flex flex-column">
+                <Button type="submit" color="green" className="w-100">
+                  Зарегистрироваться
+                </Button>
+                <p>
+                  У вас уже есть аккаунт?{" "}
+                  <span
+                    onClick={() => setAuthState("login")}
+                    className="text-primary"
+                    style={{ cursor: "pointer" }}
+                  >
+                    Войдите.
+                  </span>
+                </p>
+              </Group>
+            </form>
+          </>
         )}
       </Col>
     </Row>
