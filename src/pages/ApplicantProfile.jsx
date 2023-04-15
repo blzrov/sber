87f070bf9b1b6c7e5b6d470b21509741af5b0504
DaniilSkovorodnikov <skovorodnikov.danil@mail.ru>
2023-04-15 17:11:26 +0500

    Add Applicant card page

--- src/pages/ApplicantProfile.jsx
+++ src/pages/ApplicantProfile.jsx
@@ -11,13 +11,32 @@ export default function ApplicantProfile() {
       <Row>
         <Col md={3}>Картинка</Col>
         <Col md={5}>
-          <div>ФИО</div>
-          <div>О себе</div>
+          <h3>Личная информация</h3>
+          <div>Фамилия</div>
+          <div>Имя</div>
+          <div>Отчество</div>
+          <div>Возраст</div>
         </Col>
         <Col md={3}>
-          <div>ФИО</div>
-          <div>О себе</div>
+          <div>Контакты</div>
         </Col>
+      </Row>
+      <Row className="mb-2">
+        <h3>Подробнее о вас</h3>
+        <div>О себе</div>
+        <div>Ваши навыки</div>
+      </Row>
+      <Row className="mb-2">
+        <h3>Образование</h3>
+        <Col md={3}>Начало обучения</Col>
+        <Col md={3}>Конец обучения</Col>
+        <Col md={6}>Место обучения</Col>
+      </Row>
+      <Row>
+        <h3>Опыт работы</h3>
+        <Col md={3}>Начало работы</Col>
+        <Col md={3}>Конец работы</Col>
+        <Col md={6}>Описание</Col>
       </Row>
     </div>
   );
