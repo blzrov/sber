c5f82c8a531215ce47eea773f82216d2cfb869ea
DaniilSkovorodnikov <skovorodnikov.danil@mail.ru>
2023-04-16 02:28:06 +0500

    add fetching vacancies

--- src/pages/Vacancys.jsx
+++ src/pages/Vacancys.jsx
@@ -7,7 +7,7 @@ export default function Vacancys() {
   const [data, setData] = useState([]);
 
   useEffect(async () => {
-    const response = await fetch("http://100.73.198.48:8000/api/vacancys/");
+    const response = await fetch("http://100.73.198.48:8000/api/vacancies/");
     const data = await response.json();
     setData(data);
   }, []);
@@ -16,23 +16,27 @@ export default function Vacancys() {
     <div>
       <h3>Вакансии</h3>
       <SimpleGrid cols={3} spacing="xl">
-        <Card shadow="sm" padding="lg" radius="md" withBorder className="d-flex flex-column">
-          <Card.Section mb="xs">
-            <Image height={200} />
-          </Card.Section>
-          <Title order={2} mb="sm">
-            Frontend-разработка
-          </Title>
-          <Text mb="sm">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Maiores amet deleniti ullam atque dignissimos!
-            Quasi, sapiente beatae tempora cum quia ipsam unde incidunt soluta atque aperiam est voluptatum eos porro?
-          </Text>
-          <Link to="/vacancy-card/1">
-            <Button color="green" size="md">
-              Перейти
-            </Button>
-          </Link>
-        </Card>
+        {data.map((v) => (
+          <Card
+            shadow="sm"
+            padding="lg"
+            radius="md"
+            withBorder
+            className="d-flex flex-column"
+          >
+            <Card.Section mb="xs">
+              <Image height={200} />
+            </Card.Section>
+            <Title order={2}>{v.name}</Title>
+            <Text mb="xs">{v.salary} р/мес</Text>
+            <Text mb="sm">{v.descr}</Text>
+            <Link to="/vacancy-card/1">
+              <Button color="green" size="md">
+                Перейти
+              </Button>
+            </Link>
+          </Card>
+        ))}
       </SimpleGrid>
     </div>
   );
