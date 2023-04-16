7dcf18d02f8723876af6af1d62e7993b6d5d63b1
DaniilSkovorodnikov <skovorodnikov.danil@mail.ru>
2023-04-16 10:41:48 +0500

    commit

--- src/pages/ApplicantProfile.jsx
+++ src/pages/ApplicantProfile.jsx
@@ -1,7 +1,7 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { useNavigate } from "react-router-dom";
-import { Button } from "@mantine/core";
+import { Badge, Button } from "@mantine/core";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { getUser } from "../helpers/user";
@@ -20,47 +20,47 @@ export default function ApplicantProfile() {
 
   return (
     <div>
-      <Row>
+      <Row className="d-flex justify-content-between">
         <Col md={3}>
           <div>
-            <img src={info.img} height={200} />
+            <img src={info.img} height={230} style={{maxWidth: '300px'}}/>
           </div>
         </Col>
-        <Col md={9}>
-          <div className="d-flex justify-content-between">
-            <h3>Личная информация</h3>
-            {getUser()?.id == id && <Button onClick={() => navigate("/applicant/edit")}>Редактировать профиль</Button>}
-          </div>
-          <div style={{ fontSize: "20px" }}>{`${info.surname} ${info.name} ${info.patronymic}`}</div>
-          <div style={{ color: "gray" }}>Возраст</div>
+        <Col md={9} style={{paddingLeft: '30px'}}>
+          <Row>
+            <Col>
+              <div className="d-flex justify-content-between">
+                <h3>Личная информация</h3>
+                {getUser()?.id == id && (
+                  <Button onClick={() => navigate("/applicant/edit")}>
+                    Редактировать профиль
+                  </Button>
+                )}
+              </div>
+              <div
+                style={{ fontSize: "20px" }}
+              >{`${info.surname} ${info.name} ${info.patronymic}`}</div>
+              <div style={{ color: "gray" }}>
+                Возраст: {new Date().getFullYear() - 2003} лет
+              </div>
+            </Col>
+          </Row>
+          <Row className="mb-2">
+            <h3>Подробнее о вас</h3>
+            <div>
+              <b>О себе:</b> {info.descr}
+            </div>
+            <div>
+              <b>Ваши навыки: {['HTML', 'CSS', 'JavaScript', 'TypeScript','React'].map((v,i) => <Badge size="md" style={{marginLeft: '10px'}}>{v}</Badge>)}</b>
+            </div>
+            <div>
+              <b>Город:</b> {info.city}
+            </div>
+            <div>
+              <b>Профессия:</b> {info.profession}
+            </div>
+          </Row>
         </Col>
-      </Row>
-      <Row className="mb-2">
-        <h3>Подробнее о вас</h3>
-        <div>
-          <b>О себе:</b> {info.descr}
-        </div>
-        <div>
-          <b>Ваши навыки:</b>
-        </div>
-        <div>
-          <b>Город:</b> {info.city}
-        </div>
-        <div>
-          <b>Профессия:</b> {info.profession}
-        </div>
-      </Row>
-      <Row className="mb-2">
-        <h3>Образование</h3>
-        <Col md={3}>Начало обучения</Col>
-        <Col md={3}>Конец обучения</Col>
-        <Col md={6}>Место обучения</Col>
-      </Row>
-      <Row className="mb-2">
-        <h3>Опыт работы</h3>
-        <Col md={3}>Начало работы</Col>
-        <Col md={3}>Конец работы</Col>
-        <Col md={6}>Описание</Col>
       </Row>
     </div>
   );
